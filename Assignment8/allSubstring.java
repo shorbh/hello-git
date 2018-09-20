33f7a780b3777fc93ae15363f7f4ed8a200a1b79
saurabh <saurabhsinghal98@outlook.com>
2018-09-20 10:59:57 +0530

    by saurabh

--- Assignment8/allSubstring.java
+++ Assignment8/allSubstring.java
@@ -1,7 +1,7 @@
 import java.util.*;
 public class allSubstring {
     public static void main(String args[]) {
-        String x  = "xyz";int y = x.length();int z = 1;
+        String x  = "abcde";int y = x.length();int z = 1;
         for(int i=0;i<=y-1;i++){
             System.out.println(x.substring(i,i+z));
             if(i+z!=y) continue;
